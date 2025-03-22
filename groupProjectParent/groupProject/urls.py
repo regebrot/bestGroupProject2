babf9f8c12fd95e7ea6e78e8d3902a279c20637c
OscarTaggart <oet202@exeter.ac.uk>
2025-03-22 23:18:20 +0000

    Update urls.py

--- groupProjectParent/groupProject/urls.py
+++ groupProjectParent/groupProject/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('', include('leaderboard.urls')),
     path('', include('quizes.urls')),
     path('profile/', include('UserProfile.urls')),
+    path('bike_game/', bike_game, name="bike_game"),
     path('EnergyConservationMinigame/', include('EnergyConservationMinigame.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
