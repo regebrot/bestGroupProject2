a05508ea04779ba5e9072125bcdf752c25ffcffc
MystaMacroon <db751@exeter.ac.uk>
2025-03-24 05:29:34 +0000

    Updated Leaderboard views.py

--- groupProjectParent/leaderboard/views.py
+++ groupProjectParent/leaderboard/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from .models import leaderboard
-# Create your views here.
+from django.contrib.auth.models import User
+from django.db.models import Max
+from .models import LeaderboardEntry
+from .utils import update_badges
 
-def leaderboards(request):
-  all_members = leaderboard.objects.all().order_by('-points')
-  return render(request, "leaderboards.html", {'all': all_members})
+def leaderboard_view(request):
+    update_badges()
+    games = ["Energy Conservation", "Recycling", "Cycling", "Quiz"]
+    
+    leaderboard_data = []
+    
+    users = User.objects.filter(leaderboardentry__isnull=False).distinct()
+    for user in users:
+        display_name = user.profile.display_name if user.profile.display_name else user.username
+        user_row = {"display_name": display_name}
+        overall = 0
+        for game in games:
+            best_score = LeaderboardEntry.objects.filter(user=user, game=game).aggregate(Max('score'))['score__max'] or 0
+            user_row[game] = best_score
+            overall += best_score
+        user_row["overall"] = overall
+        leaderboard_data.append(user_row)
+
+    leaderboard_data.sort(key=lambda x: x["overall"], reverse=True)
+    
+    context = {
+        "leaderboard_data": leaderboard_data,
+        "games": games
+    }
+    return render(request, "leaderboards.html", context)
